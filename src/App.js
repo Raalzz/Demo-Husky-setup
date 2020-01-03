61aa2254b1a95f1bd357784e42560ee234da8a0c
Rahul Sawant <rahulsawanth06@gmail.com>
2020-01-03 12:54:13 +0530

    Added more scripts in package.json file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 
 function App() {
-  a=10;
-  const a;
+  //below code is just to check if validate script is running fine or not with respect to eslint.
+  // const showNames = () => {
+  //   var p = {
+  //     p1: "value1",
+  //     p2: "value2",
+  //     p3: "value3",
+  //   };
+
+  //   for (var key in p) {
+  //     if (p.hasOwnProperty(key)) {
+  //       console.log(key + " -> " + p[key]);
+  //     }
+  //   }
+  // }
+
   return (
     <div className="App">
       <h1>Dummy Project with Husky Setup</h1>
@@ -11,3 +24,9 @@ function App() {
 }
 
 export default App;
+
+//Confused with below code. Should i replace below code with the code in .lintstagedrc file.?
+// {
+//   "*.{js,jsx}": ["npm run lint:fix --", "git add"],
+//   "*.{js,jsx,json}": ["prettier --write","git add"]
+// }
